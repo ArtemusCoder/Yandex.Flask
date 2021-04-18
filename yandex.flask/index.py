d4b40d1cb7b65b6688a3fd1db05044c28820b9f2
artemusCoder <temaivanov2003@gmail.com>
2021-04-19 00:30:21 +0300

    Flask5.0

--- yandex.flask/index.py
+++ yandex.flask/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -37,6 +38,13 @@ def auto_answer():
     param['motivation'] = 'посмотрел фильм Марсианин, захотел побывать там'
     param['ready'] = True
     return render_template('auto_answer.html', **param)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
