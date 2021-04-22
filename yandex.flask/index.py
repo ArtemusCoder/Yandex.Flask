2ccfae7326e37f34939e395c62a2ff6352f6540c
artemusCoder <temaivanov2003@gmail.com>
2021-04-22 21:33:11 +0300

    Flask_Register

--- yandex.flask/index.py
+++ yandex.flask/index.py
@@ -4,6 +4,7 @@ import os
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -95,6 +96,38 @@ def gallery():
 @app.route('/member')
 def member():
     pass
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    db_session.global_init("db/database.db")
+    db_sess = db_session.create_session()
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data,
+
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
