7d5ee71a9f2a3933f3d91ec4fe0d614eae32996d
artemusCoder <temaivanov2003@gmail.com>
2021-04-26 22:19:54 +0300

    Flask.RESTful-jobs

--- yandex.flask/index.py
+++ yandex.flask/index.py
@@ -16,11 +16,14 @@ from flask_restful import reqparse, abort, Api, Resource
 
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import users_resource
+import jobs_resource
 
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(users_resource.UsersListResource, '/api/v2/users')
 api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:user_id>')
+api.add_resource(jobs_resource.JobsListResource, '/api/v2/jobs')
+api.add_resource(jobs_resource.JobsResource, '/api/v2/jobs/<int:job_id>')
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
